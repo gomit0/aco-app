ce57ddc71835f9f53d0174517be5ed1d490184f3
Carlos Alvarez <racnive@gmail.com>
2022-07-16 20:55:55 -0400

    v0.0711
    agregando navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+export default function Navbar(){
+    return <nav className="nav">
+    <a className="nav-link" href='#'>ACO Herramientas</a>
+    <a className="nav-link" href="#">Ofertas</a>
+    <a className="nav-link" href="#">Carrito</a>
+    <a className="nav-link" href='#'>Nosotros</a>
+  </nav>
+}
